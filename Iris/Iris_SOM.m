7cfb470f4d4a04041a064349a0ab8b9984dc5d2d
TastyMeat <65907415+TastyMeat@users.noreply.github.com>
2023-04-07 14:34:45 -0700

    renames

--- Iris/Iris_SOM.m
+++ Iris/Iris_SOM.m
@@ -5,7 +5,7 @@ load fisheriris.mat
 x = meas';
 testDimensions = 2:10;
 results = zeros(length(testDimensions), 1);
-meanElementPerCluster = zeros(length(testDimensions), 1);
+meanElementPerNode = zeros(length(testDimensions), 1);
 nets = cell(length(testDimensions), 1);
 
 for testId = 1 : length(testDimensions)
@@ -18,7 +18,7 @@ for testId = 1 : length(testDimensions)
     classes = vec2ind(y)';
 
     %% Test the Network
-    clusterLabels = cell(power(testDimensions(testId), 2), 1);
+    nodeLabels = cell(power(testDimensions(testId), 2), 1);
     elementsInClasses = zeros(power(testDimensions(testId), 2), 1);
     for i = 1 : power(testDimensions(testId), 2)
         elementIdsInClass = find(classes == i);
@@ -28,18 +28,18 @@ for testId = 1 : length(testDimensions)
         end
 
         % Mode for cellstr
-        clusterUniques = unique(species(elementIdsInClass));
-        frequencies = zeros(length(clusterUniques), 1);
-        for j = 1 : length(clusterUniques)
-            frequencies(j) = length(strcmp(species(elementIdsInClass), clusterUniques{j}));
+        nodeUniques = unique(species(elementIdsInClass));
+        frequencies = zeros(length(nodeUniques), 1);
+        for j = 1 : length(nodeUniques)
+            frequencies(j) = length(strcmp(species(elementIdsInClass), nodeUniques{j}));
         end
         [~, modeId] = max(frequencies);
-        clusterLabels{i} = clusterUniques{modeId};
+        nodeLabels{i} = nodeUniques{modeId};
     end
 
     %% Record the Test Results
-    results(testId) = sum(strcmp(clusterLabels(classes), species)) / length(species);
-    meanElementPerCluster(testId) = mean(elementsInClasses);
+    results(testId) = sum(strcmp(nodeLabels(classes), species)) / length(species);
+    meanElementPerNode(testId) = mean(elementsInClasses);
 end
 clear testId i j
 clear frequencies modeId
@@ -49,11 +49,11 @@ figure("Name", 'Performance of SOM on Iris Data Set');
 
 accuracyPlot = plot(power(testDimensions, 2), results, 'xb-');
 hold on
-ElementPerClusterPlot = plot(power(testDimensions, 2), meanElementPerCluster / length(species), 'xr-');
+ElementPerNodePlot = plot(power(testDimensions, 2), meanElementPerNode / length(species), 'xr-');
 hold off
 title('Performance of SOM on Iris Data Set');
 xlabel('Number of nodes');
-legend({'Accuracy', 'ElementPerClusterRatio'}, "Location", "east");
+legend({'Accuracy', 'ElementPerNodeRatio'}, "Location", "east");
 
 % Plots
 %figure, plotsomtop(net)
